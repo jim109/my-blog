6be19f37012ca3fc0f920011a00f066f970f3a1c
jim109 <josma109@gmail.com>
2015-11-07 09:27:19 -0500

    Solucionado problema de aprobar commentarios y eliminar

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,8 @@ class Post(models.Model):
 		return self.title
 
 
+
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
@@ -32,3 +34,4 @@ class Comment(models.Model):
 
     def approved_comments(self):
     	return self.comments.filter(approved_comment=True)
+
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post 
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators	import	login_required
 
