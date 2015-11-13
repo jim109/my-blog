24b6dfc5fca23ab69dc20f731efb0ecf65058a79
jim109 <josma109@gmail.com>
2015-11-13 09:29:07 -0500

    Actualizacion del wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+"""import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+application = get_wsgi_application()"""
+
+    import os
+    import sys
+
+    path = '/home/my-blog'  # aquí utiliza tu propio usuario
+    if path not in sys.path:
+        sys.path.append(path)
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+    from django.core.wsgi import get_wsgi_application
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(get_wsgi_application())
